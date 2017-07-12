0cc3ae6abdf71b4cfbb0f6e0eeb2ef770b725427
tarowookjin <tarowj05@gmail.com>
2017-07-11 21:17:55 -0500

    proyecto C

---
+++ main_proyecto.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+//Variables que se utilizaran
+typedef struct{
+    char nombre_datos[30];
+    int moda;
+    int mediana;
+    float med_arimetica;
+    float varianza;
+    float des_estandar;
+}guardar_datos;//estructura para guardar los datos
+
